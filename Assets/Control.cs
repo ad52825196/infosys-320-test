ca0dde247d5bdbc130b1414cbaa31d2d95a00139
Zhen Chen <me@zhen-chen.com>
2016-10-17 19:06:54 +1300

    Test submit

--- Assets/Control.cs
+++ Assets/Control.cs
@@ -8,16 +8,16 @@ public class Control : MonoBehaviour {
     public string Website;
     public GameObject sphere;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         Website += "?zumo-api-version=2.0.0";
         StartCoroutine(GetData());
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
 
     IEnumerator GetData() {
         UnityWebRequest www = UnityWebRequest.Get(Website);
@@ -36,17 +36,17 @@ public class Control : MonoBehaviour {
         Point[] points = JsonReader.Deserialize<Point[]>(json);
 
         foreach (Point point in points) {
-            Debug.Log("The Reading ID is: " + point.ReadingID);
+            Debug.Log("The Tree ID is: " + point.TreeID);
             coordinate = new Vector3(float.Parse(point.X), float.Parse(point.Y), float.Parse(point.Z));
-            newObject = (GameObject) Instantiate(sphere, coordinate, Quaternion.Euler(0, 180, 0));
+            newObject = (GameObject) Instantiate(sphere, coordinate, Quaternion.Euler(0, 0, 0));
             for (int i = 0; i < newObject.transform.childCount; i++) {
                 child = newObject.transform.GetChild(i).gameObject;
                 switch (child.name) {
                     case "Label":
-                        child.GetComponent<TextMesh>().text = String.Format("Label ID: {0}", point.ReadingID);
+                        child.GetComponent<TextMesh>().text = String.Format("**Label** Tree ID: {0}", point.TreeID);
                         break;
                     case "Data":
-                        child.GetComponent<TextMesh>().text = String.Format("Data:" + Environment.NewLine + "Location: {0}" + Environment.NewLine + "Safety Category: {1}  Safety Measure: {2}" + Environment.NewLine + "X: {3}  Y: {4}  Z: {5}", point.Location, point.SafetyCategory, point.SafetyMeasure, point.X, point.Y, point.Z);
+                        child.GetComponent<TextMesh>().text = String.Format("Data:" + Environment.NewLine + "Location: {0}" + Environment.NewLine + "Ecological Value: {1}  Historical Significance: {2}" + Environment.NewLine + "X: {3}  Y: {4}  Z: {5}", point.Location, point.EcologicalValue, point.HistoricalSignificance, point.X, point.Y, point.Z);
                         break;
                 }
             }
--- Assets/Point.cs
+++ Assets/Point.cs
@@ -4,11 +4,11 @@ using System.Linq;
 using System.Text;
 
 public class Point {
-    public string ReadingID { get; set; }
+    public string TreeID { get; set; }
     public string WhenReadingRecorded { get; set; }
     public string Location { get; set; }
-    public string SafetyCategory { get; set; }
-    public string SafetyMeasure { get; set; }
+    public string EcologicalValue { get; set; }
+    public string HistoricalSignificance { get; set; }
     public string X { get; set; }
     public string Y { get; set; }
     public string Z { get; set; }
